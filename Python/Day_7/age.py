d167b35d713813407b0543efeb8ede01977cc53d
gvvnr <you@example.com>
2018-12-05 11:39:15 +0530

    commited:

--- Python/Day_7/age.py
+++ Python/Day_7/age.py
@@ -1,3 +1,9 @@
+'''
+#Create a user defined exception class
+#Read input from user and if age is less than print exception that not eleigible
+#if age is greater than 21 print eligible
+#implement the above if condition in try catch
+'''
 import sys
 class Invalid(Exception):
    """You Entered an Invalid age:"""
